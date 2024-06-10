e503c946af548def64a792dd09b00c74d54bb3d4
lucky <laxmansomireddi@gmail.com>
2024-06-10 07:16:13 -0400

    laxman react files

---
+++ bsjscript.js
@@ -0,0 +1,82 @@
+
+ function enter(){
+  alert("hai") 
+ }
+
+  var chrome="wonderfull performance"
+
+ // let edge="better"
+
+  //const safari="average"
+
+  if( chrome=="wonderfull performance"){
+    document.write(chrome)
+  }
+  else if(edge="better") {
+  document.write("averge ")    
+  }
+  else if(safari="average"){
+    document.write("awesome of the planme")
+
+  }
+  else{
+    document.write("correct age to drive  cycle")
+  }
+
+
+
+  var a= 20
+  var b= 40
+  var c= 70
+
+  if(a==b){
+    document.write("equals the a and b")
+}
+else if(b>=c)
+    {
+        document.write("blue colour")
+
+}
+else if(c<=a)
+    {
+        document.write("red colour")
+    }
+
+    else{
+        document.write( "all are wrong")
+    }
+
+    let gender="male"
+    var sex="female"
+
+    var age=20
+if(age==20){
+  if(gender=="female"){
+    document.write("go to 4th floor")
+  }
+  else if(sex=="female"){
+    document.write("go to 3rd floor")
+  }
+  else{
+    document.write("go to the fifth floor")
+  }
+}
+else {
+  document.write ("there is no chiledrn")
+}
+
+
+var chrome= "wonderful performance"
+let edge= "average"
+var safari= "better"
+
+switch (browse) {
+  case chrome: 
+
+
+
+default: 
+    break;
+}
+    
+
---
+++ practise.js
@@ -0,0 +1,75 @@
+
+
+/*var name=['bhabulai','rajesh','garuda']
+
+for(i=0;i<=name.length;i++)
+    {
+        document.write(name[i])
+    }*/
+
+
+  /*  var customer=new Array();
+    customer[0]="kohli";
+    customer[1]="dhoni";
+    customer[2]="sachin";
+    customer[3]="yuvaraj";
+    customer[4]="panth";
+
+    console.log(customer)*/
+
+    /*for(i=0;i<=customer.length;i++ )
+        {
+            document.write(customer [i] )
+
+        }*/
+
+
+
+        /*
+         
+        <h3> push mentod<h3>
+        let movies=["bahubali","rrr","eega","simhadri"]
+        movies.push("maryadaramanna")
+
+        console.log(movies);*/
+
+      /* 
+      
+      <h3> pop mentod<h3>
+      let ramcharan=["orange","magadheera","nayak","dill"]
+        ramcharan.pop("dill")
+         console.log(ramcharan);*/
+
+
+         
+         /*
+          <h3> indexOf mentod<h3>
+         
+         let course=['react','javascript','html','css','nodejs']
+         
+
+         console.log(course.indexOf('css'))*/
+
+        
+
+         /*let ramcharan=["js","node","react","html","css"]
+
+         let nandhi=ramcharan.slice(1,4)
+         console.log(nandhi)*/
+
+
+         /*let myNumber=[0,1,2,3,4,5,6,7,8]
+
+         let newNumber=myNumber.splice(5,1,"new rule");
+
+         document.write(myNumber);*/
+
+
+         let fruits=['apple','babanana','orange','grapes']
+
+         fruits.forEach(function(item,index) {
+            document.write(index +"."+item+"<br>");
+        })
+            
+         
+
