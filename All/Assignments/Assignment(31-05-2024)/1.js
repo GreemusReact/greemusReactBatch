dd84bbcc2bb25f8946ea0fe5491490bff4c6f595
Nithishkumar007 <potlapula.nithish@gmail.com>
2024-06-11 17:29:11 +0530

    delete

--- All/Assignments/Assignment(31-05-2024)/1.js
+++
@@ -1,8 +0,0 @@
-let array=[1,2,3,4,5];
-console.log("Original array:"+array);
-array.push(6);
-console.log("After adding number at end:"+array);
-array.shift();
-console.log("After removing first number:"+array);
-console.log("Final array is:");
-console.log(array);
