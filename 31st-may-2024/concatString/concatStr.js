6f9976db2ea0dae699f5df81edae28a9f4e49123
venky <vmaicherla09@gmail.com>
2024-06-11 11:11:11 +0530

    array methods

--- 31st-may-2024/concatString/concatStr.js
+++ 31st-may-2024/concatString/concatStr.js
@@ -1,18 +1,10 @@
-// function concatStr(strArr) {
-//   let concantString = "";
-//   for (i = 0; i < strArr.length; i++) {
-//     concantString = concantString.concat(strArr[i]);
-//   }
-//   return concantString;
-// }
-// let str = ["java", "script", " ", "is a", " ", "scripting", " ", "language"];
-// console.log(concatStr(str));
-function conact(strArr) {
-  let concatStr = "";
+function concatStr(strArr) {
+  let concantString = "";
   for (i = 0; i < strArr.length; i++) {
-    concatStr = concatStr.concat(strArr[i]);
+    concantString = concantString.concat(strArr[i]);
   }
-  console.log(concatStr);
+  return concantString;
 }
-let arr1 = ["laxman", " ", "somireddi"];
-conact(arr1);
+let str = ["java", "script", " ", "is a", " ", "scripting", " ", "language"];
+console.log(concatStr(str));
+
