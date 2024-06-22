04649df5dcdd4215377dbc7150d7ebc43094c209
Nithish <potlapula.nithish@gmail.com>
2024-06-22 20:19:54 +0530

    update

--- Assignments/Assignment(31-05-2024)/14.js
+++
@@ -1,9 +0,0 @@
-let array=["Mango","Dog","Hotel","Apple","India"];
-function Sorted(a){
-    a.sort();
-    console.log("After sorting array is:");
-    console.log(array);
-}
-console.log("Array is:");
-console.log(array);
-Sorted(array);
